807a3e8e260dffbec115b1f36f7cd6a38090dd88
Suraj Jha <51950219+hbksj@users.noreply.github.com>
2024-10-16 05:00:26 +0530

    boto3 resource
    boto3 resource

--- pages/2_Voice_Generator.py
+++ pages/2_Voice_Generator.py
@@ -20,7 +20,12 @@ session = boto3.Session(
     region_name=os.getenv("AWS_DEFAULT_REGION"),
 )
 
-client = session.client("polly")
+client = boto3.resource(
+    "polly",
+    aws_access_key_id=os.getenv("AWS_ACCESS_KEY_ID"),
+    aws_secret_access_key=os.getenv("AWS_SECRET_ACCESS_KEY"),
+    region_name=os.getenv("AWS_DEFAULT_REGION"),
+)
 
 if "voice_engine" not in st.session_state:
     st.session_state.voice_engine = "generative"
