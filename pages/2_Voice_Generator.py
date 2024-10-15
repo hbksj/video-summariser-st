b67c18b7ddbd29c08217ef6a4b678a087d2ede3d
Suraj Jha <51950219+hbksj@users.noreply.github.com>
2024-10-16 05:01:05 +0530

    changes revert for boto
    changes revert for boto

--- pages/2_Voice_Generator.py
+++ pages/2_Voice_Generator.py
@@ -20,12 +20,7 @@ session = boto3.Session(
     region_name=os.getenv("AWS_DEFAULT_REGION"),
 )
 
-client = boto3.resource(
-    "polly",
-    aws_access_key_id=os.getenv("AWS_ACCESS_KEY_ID"),
-    aws_secret_access_key=os.getenv("AWS_SECRET_ACCESS_KEY"),
-    region_name=os.getenv("AWS_DEFAULT_REGION"),
-)
+client = session.client("polly")
 
 if "voice_engine" not in st.session_state:
     st.session_state.voice_engine = "generative"
