4dfba2babf6db3f3b28084aa1907043b5e17d969
Suraj Jha <51950219+hbksj@users.noreply.github.com>
2024-10-16 05:01:44 +0530

    print key
    print key

--- pages/2_Voice_Generator.py
+++ pages/2_Voice_Generator.py
@@ -14,6 +14,7 @@ import os
 load_dotenv()
 
 
+print(os.getenv("AWS_ACCESS_KEY_ID"),)
 session = boto3.Session(
     aws_access_key_id=os.getenv("AWS_ACCESS_KEY_ID"),
     aws_secret_access_key=os.getenv("AWS_SECRET_ACCESS_KEY"),
